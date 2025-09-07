4e9099e5d3456b0b2ae47641f2436f8db2eb9dfa
Sut <xepinchan@qq.com>
2025-09-07 17:20:44 +0800

    优化菜单项开关

--- wpcleanadmin/assets/js/wpca-settings.js
+++ wpcleanadmin/assets/js/wpca-settings.js
@@ -19,19 +19,34 @@ jQuery(document).ready(function($) {
     // Menu sorting functionality
     // ======================================================
     
-    // Make all menu items sortable in a flat list
+    // Make all menu items sortable with hierarchy support
     $('#wpca-menu-order').sortable({
         items: 'li',
         handle: '.dashicons-menu',
+        tolerance: 'pointer',
         update: function(event, ui) {
-            // Update menu order
-            var menuOrder = [];
-            $('#wpca-menu-order li').each(function() {
-                menuOrder.push($(this).data('menu-slug'));
-            });
-            $('#wpca_menu_order').val(JSON.stringify(menuOrder));
+            updateMenuOrder();
         }
     });
+
+    // Update menu order and hidden status
+    function updateMenuOrder() {
+        var menuOrder = [];
+        var hiddenItems = [];
+        
+        $('#wpca-menu-order li').each(function() {
+            var $item = $(this);
+            var slug = $item.data('menu-slug');
+            menuOrder.push(slug);
+            
+            if ($item.hasClass('menu-hidden')) {
+                hiddenItems.push(slug);
+            }
+        });
+        
+        $('#wpca_menu_order').val(JSON.stringify(menuOrder));
+        $('#wpca_hidden_items').val(JSON.stringify(hiddenItems));
+    }
 
     // Reset menu order to default
     $('#wpca-reset-menu-order').click(function() {
@@ -66,6 +81,106 @@ jQuery(document).ready(function($) {
             $('input[name="wpca_settings[submenu_order]['+parentSlug+'][]"]').val(JSON.stringify(submenuOrder));
         }
     });
+
+    // ======================================================
+    // Enhanced menu functionality
+    // ======================================================
+    
+    // Handle toggle switch changes for all menu items
+    $(document).on('change', '.wpca-slide-toggle input', function() {
+        var $li = $(this).closest('li');
+        $li.toggleClass('menu-hidden', this.checked);
+        
+        // Toggle child submenus as well
+        $li.find('> ul li').each(function() {
+            $(this).toggleClass('menu-hidden', this.checked);
+        });
+        
+        updateMenuOrder();
+    });
+    
+    // Full hierarchical menu initialization
+    function initializeMenuHierarchy() {
+        // First show all menu containers
+        $('#wpca-menu-order, .wpca-submenu-sortable').css('display', 'block');
+        
+        // Process all menu items
+        $('#wpca-menu-order').find('li').each(function() {
+            var $item = $(this);
+            var slug = $item.data('menu-slug');
+            var isHidden = $item.hasClass('menu-hidden');
+            var level = $item.data('level') || 0;
+            var isSubmenu = $item.hasClass('submenu-item');
+            var hasChildren = $item.find('> ul').length > 0;
+            
+            // Set item styling
+            $item.css({
+                'padding-left': (level * 25) + 'px',
+                'display': 'block',
+                'position': 'relative'
+            });
+            
+            // Add toggle switch if missing
+            if (!$item.find('.wpca-slide-toggle').length) {
+                $item.append(`
+                    <label class="wpca-slide-toggle" style="position:absolute; right:10px;">
+                        <input type="checkbox" name="wpca_settings[menu_hidden][${slug}]" 
+                            ${isHidden ? 'checked' : ''}>
+                        <span class="slider">
+                            <span class="slide-handle"></span>
+                        </span>
+                    </label>
+                `);
+            }
+            
+            // Ensure child UL is visible for parent items
+            if (hasChildren) {
+                $item.find('> ul').css({
+                    'display': 'block',
+                    'margin-left': '15px'
+                });
+            }
+            
+            // Add expand/collapse icon for parent items
+            if (hasChildren && !$item.find('.wpca-menu-expand').length) {
+                $item.append(`
+                    <span class="dashicons dashicons-arrow-down wpca-menu-expand" 
+                          style="position:absolute; right:10px; cursor:pointer;"></span>
+                `);
+            }
+        });
+        
+        // Toggle child menu visibility
+        $(document).on('click', '.wpca-menu-expand', function() {
+            $(this).toggleClass('dashicons-arrow-down dashicons-arrow-up')
+                   .siblings('ul').toggle();
+        });
+    }
+    
+    // Initialize menu on load and after sorting
+    initializeMenuHierarchy();
+    $('#wpca-menu-order').on('sortupdate', initializeMenuHierarchy);
+
+    // Update menu order and hidden status
+    function updateMenuOrder() {
+        var menuOrder = [];
+        var hiddenItems = [];
+        
+        $('#wpca-menu-order li').each(function() {
+            var $item = $(this);
+            var slug = $item.data('menu-slug');
+            var level = $item.data('level') || 0;
+            $item.css('padding-left', (level * 20) + 'px');
+            menuOrder.push(slug);
+            
+            if ($item.hasClass('menu-hidden')) {
+                hiddenItems.push(slug);
+            }
+        });
+        
+        $('#wpca_menu_order').val(JSON.stringify(menuOrder));
+        $('#wpca_hidden_items').val(JSON.stringify(hiddenItems));
+    }
 
     // ======================================================
     // General admin functionality (from wp-clean-admin.js)
